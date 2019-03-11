3b3478f4c2d7c6f837afb53bf24f8f8851e2a015
jkoba <jkoba0512@gmail.com>
2019-03-11 16:19:34 +0900

    [MAKE] motor_chara_comp

---
+++ motor_chara_comp/motor_chara_comp.ino
@@ -0,0 +1,115 @@
+#include <PinChangeInt.h>
+
+// pin number
+// motor1
+const int PIN_MOTOR_DIR_1 = 2;  // dir
+const int PIN_MOTOR_PWM_1 = 3;  // pwm
+const int PIN_ENCODER_A_1 = 4;  // encoder A
+const int PIN_ENCODER_B_1 = 5;  // encoder B
+
+// pwm_com (-255 ~ 255)
+int pwm_com_1 = 0;
+
+// encorder pulse count
+volatile int enc_counting_1 = 0;  // inc/dec by interrupt handler
+volatile int enc_count_1 = 0;     // for control law
+
+// accumulate encoder pulse count for 100 ms
+float acc_enc_count_1 = 0.0;
+int counter_enc_count_1 = 0;
+
+// control period (us)
+const int T_S = 10000;
+
+// for keep control period
+long int start_time, end_time;
+
+void setup() {
+  // change PWM freq
+  TCCR1B=TCCR1B&0xf8|0x01;  // pin9,pin10 PWM 31250Hz
+  TCCR2B=TCCR2B&0xf8|0x01;  // pin3,pin11 PWM 31250Hz
+
+  // set pin mode
+  pinMode(PIN_MOTOR_PWM_1, OUTPUT);
+  pinMode(PIN_MOTOR_DIR_1, OUTPUT);
+  pinMode(PIN_ENCODER_A_1, INPUT);
+  pinMode(PIN_ENCODER_B_1, INPUT);
+
+  // set a handler for pin interrupt (for encoder pulse measurement)
+  PCintPort::attachPinChangeInterrupt(PIN_ENCODER_A_1, riseEncA1, RISING);
+
+  // serial
+  Serial.begin(9600);
+}
+
+void riseEncA1() {
+  if (digitalRead(PIN_ENCODER_B_1) == LOW) enc_counting_1++;  // CW
+  else enc_counting_1--;  // CCW
+}
+
+void loop() {
+  start_time = micros();  // start control period
+
+  // update enc_count_1
+  enc_count_1 = enc_counting_1;
+  enc_counting_1 = 0;
+
+  // set direction
+  if (pwm_com_1 >= 0) {
+    digitalWrite(PIN_MOTOR_DIR_1, 0);  // CW
+  }
+  else {
+    digitalWrite(PIN_MOTOR_DIR_1, 1);  // CCW
+  }
+
+  // output pwm
+  analogWrite(PIN_MOTOR_PWM_1, abs(pwm_com_1));
+
+  // process key inputs
+  if (Serial.available() > 0) processSerialKeyInput();
+
+  // accumulate enc_count_1 for 100 ms
+  acc_enc_count_1 += enc_count_1;
+  counter_enc_count_1++;
+
+  // each 100 ms
+  if (counter_enc_count_1 == 10) {
+    // print pwm_com_1, enc_count_1
+    Serial.print(pwm_com_1);
+    Serial.print(" ");
+    Serial.println(acc_enc_count_1 / 10);  // averaging enc_count_1 for 100 ms
+    counter_enc_count_1 = 0;
+  }
+
+  // for keep control period
+  end_time = micros();
+  delayMicroseconds(max(0, start_time + T_S - end_time));
+}
+
+int limitCOM(int _com) {
+  if (_com >  255) return 255;
+  if (_com < -255) return -255;
+  return _com;
+}
+
+void processSerialKeyInput() {
+  switch (Serial.read()) {
+    case 'a':
+      pwm_com_1 = limitCOM(pwm_com_1 + 10);
+      break;
+    case 's':
+      pwm_com_1 = 0;
+      break;
+    case 'd':
+      pwm_com_1 = limitCOM(pwm_com_1 - 10);
+      break;
+    case 'w':
+      pwm_com_1 = 255;
+      break;
+    case 'x':
+      pwm_com_1 = - 255;
+      break;
+    default:
+      ;
+  }
+}
